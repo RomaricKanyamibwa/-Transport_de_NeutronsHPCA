df9facba43e3750d145016bdda426c2635ef4e28
haroldGal <harold.gallice@gmail.com>
2018-10-25 18:03:16 +0200

    avancee gpu manque random et atomic

--- CUDA/neutron-para.cu
+++ CUDA/neutron-para.cu
@@ -52,32 +52,44 @@ double my_gettimeofday(){
   return tmp_time.tv_sec + (tmp_time.tv_usec * 1.0e-6L);
 }
 
-__global__ void neutron_calculus(curandState *state, float c, float c_c, float h, float* absorbed, int r, int b, int t){
+__global__ void setup_kernel(curandState *state)
+{
+    int id = threadIdx.x + blockIdx.x*gridDim.x;
+    /* Each thread gets same seed, a different sequence 
+       number, no offset */
+    curand_init(1234, id, 0, &state[id]);
+}
+
+__global__ void neutron_calculus(curandState *state, float c, float c_c, float h, float* absorbed, int* result, int n){
     int pos_Thread = threadIdx.x + blockIdx.x*blockDim.x;
     float L;
     float u;
     float d = 0.0;
     float x = 0.0;
-    while (1) {
-
-      u = curand_uniform (state + pos_Thread);
-      L = -(1 / c) * log(u);
-      x = x + L * cos(d);
-      if (x < 0) {
-	atomicAdd(&r,1);
-	break;
-      } else if (x >= h) {
-	atomicAdd(&t,1);
-	break;
-      } else if ((u = curand_uniform (state + pos_Thread)) < c_c / c) {
-	atomicAdd(&b,1);
-	absorbed[pos_Thread] = x;
-	break;
-      } else {
-	u = curand_uniform (state + pos_Thread);
-	d = u * M_PI;
-      }
-    }
+    while(pos_Thread < n) {
+	    while (1) {
+	      //result[2]=5;
+	      u = curand_uniform (&state[pos_Thread%(gridDim.x*blockDim.x)]);
+	      L = -(1 / c) * log(u);
+	      x = x + L * cos(d);
+	      if (x < 0) {
+		atomicAdd(result,1);
+		break;
+	      } else if (x >= h) {
+		atomicAdd(result+1,1);
+		break;
+	      } else if ((u = curand_uniform (&state[pos_Thread%blockDim.x])) < c_c / c) {
+		atomicAdd(result+2,1);
+	
+		absorbed[pos_Thread] = x;
+		break;
+	      } else {
+		u = curand_uniform (state + pos_Thread);
+		d = u * M_PI;
+	      }
+	    }
+	pos_Thread = pos_Thread + gridDim.x*blockDim.x;
+	}
 }
 /*
  * main()
@@ -99,7 +111,7 @@ int main(int argc, char *argv[]) {
   // nombre d'échantillons
   int n;
   // nombre de neutrons refléchis, absorbés et transmis
-  int r, b, t;
+  int* result = (int *) calloc(3, sizeof(int)); //r, b, t
   // chronometrage
   double start, finish;
   int i, j = 0; // compteurs 
@@ -109,7 +121,7 @@ int main(int argc, char *argv[]) {
 
   // valeurs par defaut
   h = 1.0;
-  n = 500000000;
+  n = 50000;
   c_c = 0.5;
   c_s = 0.5;
 
@@ -122,7 +134,6 @@ int main(int argc, char *argv[]) {
     c_c = atof(argv[3]);
   if (argc > 4)
     c_s = atof(argv[4]);
-  r = b = t = 0;
   c = c_c + c_s;
 
   // affichage des parametres pour verificatrion
@@ -134,15 +145,23 @@ int main(int argc, char *argv[]) {
 
   float *absorbed;
   absorbed = (float *) calloc(n, sizeof(float));
-  int nb_thread;
+  int nb_thread = 256;
   dim3 threadsParBloc(nb_thread,1,1);
-  dim3 nbBlocks(n/threadsParBloc.x,1,1);
+  dim3 nbBlocks(256,1,1);
   float* absorbed_gpu;
+  int* result_gpu;
   curandState* d_state;
+  cudaMalloc(&d_state, nb_thread*nbBlocks.x*sizeof(curandState));
   cudaMalloc(&absorbed_gpu, n*sizeof(float));
+  cudaMalloc(&result_gpu, 3*sizeof(int));
+
+  cudaMemcpy(result_gpu, result, 3*sizeof(int),cudaMemcpyHostToDevice);
+
   start = my_gettimeofday();
-  neutron_calculus<<<nbBlocks, threadsParBloc >>>(d_state, c, c_c, h, absorbed_gpu, r, b, t);
+  setup_kernel<<<nbBlocks, threadsParBloc >>>(d_state);
+  neutron_calculus<<<nbBlocks, threadsParBloc >>>(d_state, c, c_c, h, absorbed_gpu, result_gpu, n);
   cudaMemcpy(absorbed, absorbed_gpu, n*sizeof(float),cudaMemcpyDeviceToHost);
+  cudaMemcpy(result, result_gpu, 3*sizeof(int),cudaMemcpyDeviceToHost);
   finish = my_gettimeofday();
   printf("\nTemps total de calcul: %.8g sec\n", finish - start);
 
@@ -180,6 +199,9 @@ int main(int argc, char *argv[]) {
   // fin du chronometrage
   finish = my_gettimeofday();
   */
+  int r = result[0];
+  int b = result[1];
+  int t = result[2];
   printf("\nPourcentage des neutrons refléchis : %4.2g\n", (float) r / (float) n);
   printf("Pourcentage des neutrons absorbés : %4.2g\n", (float) b / (float) n);
   printf("Pourcentage des neutrons transmis : %4.2g\n", (float) t / (float) n);
